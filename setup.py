83882ab8b808b805f077adf6ec85a6c3bf6a9e13
swapnil sonawane <sswapnil0098@gmail.com>
2021-09-09 11:03:39 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from os import name
+from setuptools import setup, find_packages
+
+setup(name="src",
+      version='0.0.1',
+      description='its a wineQ package',
+      author='swapnil sonawane',
+      packages=find_packages(),
+      license='MIT')
